519934ac070c40fddc883e15806ee09ee4dd4b7d
sessaluca <sessa.luca@iisgalvanimi.edu.it>
2019-10-22 11:55:11 +0000

    pug

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'pug');
+app.get('/', function (req, res) {
+  //res.send('PROSCIUTTO!');
+  res.render('index');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
