317d8885093567b7ba623bf78ec757fce467abcb
sessaluca <sessa.luca@iisgalvanimi.edu.it>
2019-10-24 11:33:54 +0000

    pub

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var express = require('express');
-var app = express();
+
+const express = require('express');
+const people = require('./people.json'); //Copia il file people.json dentro la variabile people
+const app = express();
+
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
-app.get('/', function (req, res) {
-  //res.send('PROSCIUTTO!');
-  res.render('index');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles //Passa il vettore profiles alla pagina index.pug
+  });
 });
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+ console.log('Example app listening on port 3000!');
 });
