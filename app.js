171f1476bc566ae2038fcf45c240d0d82a0763ac
sessaluca <sessa.luca@iisgalvanimi.edu.it>
2019-10-24 11:33:49 +0000

    pub1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
   //res.send('PROSCIUTTO!');
   res.render('index');
